56324ef19b5e656b305c38efedc83dd778c0be24
Gustavo M <gusmaldonadob@gmail.com>
2021-05-26 20:56:12 -0500

    Advance 1

--- app.py
+++ app.py
@@ -1,1 +1,33 @@
+# Imports Dependencies
+from flask import Flask, jsonify
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+import sqlalchemy
+import numpy as np
+import pandas as pd
+import datetime as dt
 
+#################################################
+
+# Database Setup
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+
+# Reflect an existing database into a new model
+Base = automap_base()
+
+# Reflect the tables
+Base.prepare(engine, reflect = True)
+
+# Save reference to the tables
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
+# Create session
+session = Session(engine)
+
+#################################################
+
+# Flask Setup
+
+app = Flask(__name__)
